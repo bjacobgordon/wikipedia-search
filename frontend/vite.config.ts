c6d2c8460bfef644038e8c2c2ad4a6daeeaf33d5
Jacob Gordon <bjacobgordon@gmail.com>
2024-10-26 09:42:36 -0500

    build(frontend): configures dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {
+  defineConfig,
+  loadEnv,
+  type UserConfig,
+} from 'vite';
+
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: './distribution',
-  },
+export default defineConfig(({ mode }) => {
+  const runningConfig: UserConfig = {
+    plugins: [react()],
+    build: {
+      outDir: './distribution',
+    },
+  }
+
+  if (mode === 'development') {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    runningConfig.server = {
+      host: true,
+      port: 3000,
+      strictPort: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      }
+    };
+  }
+
+  return runningConfig;
 })
