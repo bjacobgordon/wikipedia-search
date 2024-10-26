dbe3468e425a28bdec9b6cfad98a3410628ba7e1
Jacob Gordon <bjacobgordon@gmail.com>
2024-10-25 21:40:08 -0500

    build: adds Docker support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://backend:3001',
         changeOrigin: true,
         secure: false,
       },
